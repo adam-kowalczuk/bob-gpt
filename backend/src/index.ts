e793d2cd893505165828cc7d9498101f2e542c4b
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-10-30 15:51:10 -0700

    Add comments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,4 +2,8 @@ import express from "express";
 
 const app = express();
 
+//Middleware
+app.use(express.json());
+
+//Connection
 app.listen(8080, () => console.log("Server running on PORT 8080"));
