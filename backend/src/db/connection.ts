a1f84e8dfe88a470445b97ed08861a76b7594023
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-10-30 16:47:30 -0700

    Create async functions for db connect/disconnect

--- backend/src/db/connection.ts
+++ backend/src/db/connection.ts
@@ -1,3 +1,23 @@
-import { connect } from "mongoose";
+import { connect, disconnect } from "mongoose";
 
-export default async function connectToDatabase() {}
+async function connectToDatabase() {
+  try {
+    await connect(process.env.MONGODB_URL);
+  } catch (error) {
+    console.log(error);
+
+    throw new Error("Cannot connect to MongoDB");
+  }
+}
+
+async function disconnectFromDatabase() {
+  try {
+    await disconnect();
+  } catch (error) {
+    console.log(error);
+
+    throw new Error("Cannot Disconnect from  MongoDB");
+  }
+}
+
+export { connectToDatabase, disconnectFromDatabase };
