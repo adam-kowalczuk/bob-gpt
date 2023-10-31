73f577f561d4d17831651a1cb239714832f81b3c
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-10-30 19:06:26 -0700

    Import express app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,4 @@
-import express from "express";
-import { config } from "dotenv";
-
-config();
-const app = express();
-
-//Middleware
-app.use(express.json());
+import app from "./app.js";
 
 //Connection
 app.listen(8080, () => console.log("Server running on PORT 8080"));
