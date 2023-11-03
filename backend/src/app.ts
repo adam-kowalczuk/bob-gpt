41b46e1316b1bde5c261108a53d0bf5135a65b3f
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-11-03 10:41:21 -0700

    Import morgan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { config } from "dotenv";
+import morgan from "morgan";
 
 config();
 const app = express();
 
 //Middleware
 app.use(express.json());
+
+//Remove in production
+app.use(morgan("dev"));
 
 export default app;
