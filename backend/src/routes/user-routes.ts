9c510731745e2c51089adefcaaabcc6704ca28bc
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-11-04 15:33:15 -0700

    Create login route

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { createUser, getAllUsers } from "../controllers/user-controllers.js";
-import { signupValidator, validate } from "../utils/validators.js";
+import {
+  loginValidator,
+  signupValidator,
+  validate
+} from "../utils/validators.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers);
 userRoutes.post("/signup", validate(signupValidator), createUser);
+userRoutes.post("/login", validate(loginValidator), createUser);
 
 export default userRoutes;
