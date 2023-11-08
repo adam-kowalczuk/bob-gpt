d559c245adf70a78f2b400271035e54de9c2e920
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-11-07 16:35:39 -0800

    Move Typography component below Link

--- frontend/src/components/shared/logo.tsx
+++ frontend/src/components/shared/logo.tsx
@@ -9,7 +9,7 @@ const Logo = () => {
         display: "flex",
         marginRight: "auto",
         alignItems: "center",
-        gap: "8px"
+        gap: "15px"
       }}
     >
       <Link to={"/"}>
@@ -20,17 +20,17 @@ const Logo = () => {
           height={"30px"}
           className="image-inverted"
         />
-        <Typography
-          sx={{
-            display: { md: "block", sm: "none", xs: "none" },
-            mr: "auto",
-            fontWeight: "800",
-            textShadow: "2px 2px 20px #000"
-          }}
-        >
-          <span style={{ fontSize: "20px" }}>BOB</span>-GPT
-        </Typography>
       </Link>
+      <Typography
+        sx={{
+          display: { md: "block", sm: "none", xs: "none" },
+          mr: "auto",
+          fontWeight: "800",
+          textShadow: "2px 2px 20px #000"
+        }}
+      >
+        <span style={{ fontSize: "20px" }}>BOB</span>-GPT
+      </Typography>
     </div>
   );
 };
